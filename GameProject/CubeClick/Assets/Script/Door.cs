8bf78f24fcdf27453544bf946c7386ce30b5f198
ym <1181578156@qq.com>
2018-10-15 14:04:01 +0800

    asd

---
+++ GameProject/CubeClick/Assets/Script/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	private void OnCollisionEnter(Collision other) {
+		
+	}
+}
--- GameProject/CubeClick/Assets/Script/GameManager.cs
+++ GameProject/CubeClick/Assets/Script/GameManager.cs
@@ -7,8 +7,12 @@ public class GameManager : MonoBehaviour {
 	public GameObject[] m_LevelPrefabArr;
 	public GameObject m_WorldRoot;
 	public GameObject m_UIRoot;
-	void Start () {
 
+	private string m_CurLevelName;
+	private GameObject m_CurLevelObj;
+	void Start () {
+		HideLevel();
+		ShowPanel("MenuPanel");
 	}
 	public void ShowPanel(string _PanelName) 
 	{
@@ -28,23 +32,36 @@ public class GameManager : MonoBehaviour {
 	}
 	public void ShowLevel(string _LevelName) 
 	{
-		Transform _LevelTr = m_WorldRoot.transform.Find(_LevelName);
-		if(_LevelTr)
+		m_WorldRoot.SetActive(true);
+		Destroy(m_CurLevelObj);
+		foreach(GameObject prefab in m_LevelPrefabArr)
 		{
-			_LevelTr.gameObject.SetActive(true);
-		} 
-		else 
-		{
-			foreach(GameObject prefab in m_LevelPrefabArr)
+			if(prefab.name == _LevelName) 
 			{
-				if(prefab.name == _LevelName) 
-				{
-					GameObject obj = Instantiate(prefab);
-					obj.SetActive(true);
-					obj.transform.parent = m_WorldRoot.transform;
-				}
+				GameObject obj = Instantiate(prefab);
+				m_CurLevelName = _LevelName;
+				m_CurLevelObj = obj;
+				obj.SetActive(true);
+				obj.transform.parent = m_WorldRoot.transform;
 			}
 		}
+	}
+	public void HideLevel() 
+	{
+		m_WorldRoot.SetActive(false);
+	}
+
+	public void RestartLevel() 
+	{
+		ShowLevel(m_CurLevelName);
+	}
+	public void PauseGame()
+	{
+		Time.timeScale = 0;
+	}
+	public void ContinueGame()
+	{
+		Time.timeScale = 1;
 	}
 	// Update is called once per frame
 	void Update () {
